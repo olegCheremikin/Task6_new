9e844bf43e7d867b2401fbbc597e4b601e0df112
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-10-20 11:03:41 +0300

    Добавьте файлы проекта.

---
+++ Task6.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:\n");
+            string str = Console.ReadLine().Replace(" ", "").ToLower();
+            string temp = str;
+            string reverseStr = "";
+            for (int i = temp.Length - 1; i >= 0; i--)
+            {
+                reverseStr += temp[i];
+            }
+            if (temp == reverseStr)
+            {
+                Console.WriteLine("Введенная строка палиндром!");
+            }
+            else
+            {
+                Console.WriteLine("Введенная строка не палиндром!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: \n");
+            string a = Console.ReadLine();
+            string[] str = a.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int index = 0;
+            int max = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > index)
+                {
+                    index = str[i].Length;
+                    max = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", str[max]);
+            Console.ReadLine();
+        }
+    }
+}
